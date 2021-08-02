3b08b4f40bc7f06dafe700358b44d1a4496dd10c
dorianprogrammer <dorianrr98@gmail.com>
2021-08-02 16:27:46 -0600

    Add project files.

---
+++ Reto2/Reto2/ControlView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Reto2
+{
+    internal class ControlView : View
+    {
+        public static double WidthRequest = 133;
+        public static double HeightRequest = 260;
+        public static LayoutOptions VerticalOptionsAtTheStart = LayoutOptions.Start;
+        public static LayoutOptions VerticalOptionsAtTheCenter = LayoutOptions.Center;
+        public static LayoutOptions VerticalOptionsAtTheEnd = LayoutOptions.End;
+        public static Brush ColorOfTheBoxes = Brush.Red;
+    }
+}
---
+++ Reto2/Reto2/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Reto2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ClickOnButton(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            button.Background = Brush.Blue;
+        }
+    }
+}
